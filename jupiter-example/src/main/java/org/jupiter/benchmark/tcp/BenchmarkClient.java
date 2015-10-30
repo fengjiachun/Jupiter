b489aea0ae5f48deaf4bae50dbe292903283b30f
fengjc <fengjc@fjc-mbp.local>
2015-10-30 23:59:25 +0800

    Initial commit

--- jupiter-example/src/main/java/org/jupiter/benchmark/tcp/BenchmarkClient.java
+++ jupiter-example/src/main/java/org/jupiter/benchmark/tcp/BenchmarkClient.java
@@ -46,7 +46,7 @@ public class BenchmarkClient {
         NettyConnector connector = new JNettyTcpConnector();
         UnresolvedAddress[] addresses = new UnresolvedAddress[processors];
         for (int i = 0; i < processors; i++) {
-            addresses[i] = new UnresolvedAddress("127.0.0.1", 18099);
+            addresses[i] = new UnresolvedAddress("192.168.77.83", 18099);
             connector.connect(addresses[i]);
         }
 
@@ -65,7 +65,7 @@ public class BenchmarkClient {
             }
         }
 
-        final int t = 50000;
+        final int t = 100000;
         long start = System.currentTimeMillis();
         final CountDownLatch latch = new CountDownLatch(processors << 6);
         final AtomicLong count = new AtomicLong();
