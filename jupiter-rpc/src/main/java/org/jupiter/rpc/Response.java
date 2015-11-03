43621cc281a65ccc8a0130d4f8fdbfecf163403c
fengjc <fengjc@fjc-mbp.local>
2015-11-03 11:56:58 +0800

    Initial commit

--- jupiter-rpc/src/main/java/org/jupiter/rpc/Response.java
+++ jupiter-rpc/src/main/java/org/jupiter/rpc/Response.java
@@ -1,7 +1,5 @@
 package org.jupiter.rpc;
 
-import org.jupiter.common.util.Recyclable;
-import org.jupiter.common.util.RecycleUtil;
 import org.jupiter.rpc.model.metadata.ResultWrapper;
 
 import static org.jupiter.rpc.Status.OK;
@@ -15,7 +13,7 @@ import static org.jupiter.rpc.Status.parse;
  *
  * @author jiachun.fjc
  */
-public class Response extends BytesHolder implements Recyclable {
+public class Response extends BytesHolder {
 
     private byte status = OK.value();
 
@@ -50,17 +48,6 @@ public class Response extends BytesHolder implements Recyclable {
 
     public void result(ResultWrapper result) {
         this.result = result;
-    }
-
-    /**
-     * Response对象应在encode之后回收.
-     */
-    @Override
-    public boolean recycle() {
-        Object recyclableObject = result;
-        result = null; // 如果result对象已经逃逸出去了就没啥好办法了, 但也不会引起致命错误
-
-        return RecycleUtil.recycle(recyclableObject);
     }
 
     @Override
