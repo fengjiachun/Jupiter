3e2669265d5b08a8e89e19ebf76dadee15a76c2b
fengjc <fengjc@fjc-mbp.local>
2015-11-07 01:48:41 +0800

    To practice English

--- jupiter-transport-netty/src/main/java/org/jupiter/transport/netty/handler/connector/ConnectorIdleStateTrigger.java
+++ jupiter-transport-netty/src/main/java/org/jupiter/transport/netty/handler/connector/ConnectorIdleStateTrigger.java
@@ -21,6 +21,7 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
+import org.jupiter.transport.netty.Heartbeats;
 
 /**
  * jupiter
