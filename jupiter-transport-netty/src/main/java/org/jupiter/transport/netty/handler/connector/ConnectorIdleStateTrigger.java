b057ea01510acd557cf8a7bd79341ff7ce672702
fengjc <fengjc@fjc-mbp.local>
2015-11-07 01:48:02 +0800

    To practice English

--- jupiter-transport-netty/src/main/java/org/jupiter/transport/netty/handler/connector/ConnectorIdleStateTrigger.java
+++ jupiter-transport-netty/src/main/java/org/jupiter/transport/netty/handler/connector/ConnectorIdleStateTrigger.java
@@ -21,7 +21,6 @@ import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.timeout.IdleState;
 import io.netty.handler.timeout.IdleStateEvent;
-import org.jupiter.transport.netty.HeartBeats;
 
 /**
  * jupiter
@@ -38,7 +37,7 @@ public class ConnectorIdleStateTrigger extends ChannelInboundHandlerAdapter {
             IdleState state = ((IdleStateEvent) evt).state();
             if (state == IdleState.WRITER_IDLE) {
                 // write heartbeat to server
-                ctx.writeAndFlush(HeartBeats.heartbeatContent());
+                ctx.writeAndFlush(Heartbeats.heartbeatContent());
             }
         } else {
             super.userEventTriggered(ctx, evt);
