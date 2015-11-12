ff975f15449f9de98bb2c9ffc0f1a1c4ce496c91
fengjc <fengjc@fjc-mbp.local>
2015-11-12 22:16:40 +0800

    Add hot executor

--- jupiter-hotexec/src/main/java/org/jupiter/hot/exec/package-info.java
+++ jupiter-hotexec/src/main/java/org/jupiter/hot/exec/package-info.java
@@ -18,10 +18,10 @@
  * 客户端编译, 服务端执行, 以java的方式, 留一个方便线上调试的口子.
  *
  * 使用方式:
- *      服务端注册 {@link org.jupiter.hot.exec.JavaClassExecProvider} 作为一个provider.
+ *      1. 服务端注册 {@link org.jupiter.hot.exec.JavaClassExecProvider} 作为一个provider.
  *
- *      客户端使用 {@link org.jupiter.hot.exec.JavaCompiler} 编译需要执行的类, 将编译返回的字节码byte数组
- *      作为consumer的参数, 最后再以RPC的方式调用 {@link org.jupiter.hot.exec.JavaClassExec#exec(byte[])}.
+ *      2. 客户端使用 {@link org.jupiter.hot.exec.JavaCompiler} 编译需要执行的类, 将编译返回的字节码byte数组
+ *         作为consumer的参数, 最后再以RPC的方式调用 {@link org.jupiter.hot.exec.JavaClassExec#exec(byte[])}.
  *
  * jupiter
  * org.jupiter.hot.exec
