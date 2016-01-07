fe8956fb6573d3bb5b66ac05e73ffaab6f64d3d9
fengjc <fengjc@fjc-mbp.local>
2016-01-07 23:43:23 +0800

    支持泛化调用

--- jupiter-rpc/src/main/java/org/jupiter/rpc/consumer/GenericProxyFactory.java
+++ jupiter-rpc/src/main/java/org/jupiter/rpc/consumer/GenericProxyFactory.java
@@ -42,7 +42,7 @@ import static org.jupiter.rpc.DispatchMode.ROUND;
 /**
  * 泛化ProxyFactory
  *
- * 简单的理解, 泛化调用就是不依赖二方包, 通过传入方法名, 方法参数值, 就可以调用服务。
+ * 简单的理解, 泛化调用就是不依赖二方包, 通过传入方法名, 方法参数值, 就可以调用服务.
  *
  * jupiter
  * org.jupiter.rpc.consumer
--- jupiter-rpc/src/main/java/org/jupiter/rpc/consumer/invoker/GenericInvoker.java
+++ jupiter-rpc/src/main/java/org/jupiter/rpc/consumer/invoker/GenericInvoker.java
@@ -17,7 +17,7 @@
 package org.jupiter.rpc.consumer.invoker;
 
 /**
- * 简单的理解, 泛化调用就是不依赖二方包, 通过传入方法名, 方法参数值, 就可以调用服务。
+ * 简单的理解, 泛化调用就是不依赖二方包, 通过传入方法名, 方法参数值, 就可以调用服务.
  *
  * jupiter
  * org.jupiter.rpc.consumer.invoker
