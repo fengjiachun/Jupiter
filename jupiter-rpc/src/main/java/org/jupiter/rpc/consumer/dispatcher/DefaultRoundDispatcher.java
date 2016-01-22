1f959c171f4ea4a44f82711a2d2d23cdeafcebae
fengjc <fengjc@fjc-mbp.local>
2016-01-22 15:24:08 +0800

    Initial commit

--- jupiter-rpc/src/main/java/org/jupiter/rpc/consumer/dispatcher/DefaultRoundDispatcher.java
+++ jupiter-rpc/src/main/java/org/jupiter/rpc/consumer/dispatcher/DefaultRoundDispatcher.java
@@ -55,14 +55,7 @@ public class DefaultRoundDispatcher extends AbstractDispatcher {
         message.setAppName(proxy.appName());
         message.setMethodName(methodName);
         message.setArgs(args);
-        // tracing [允许在业务代码里设置traceId]
-        String traceId = Tracing.getCurrent();
-        if (traceId == null) {
-            traceId = Tracing.generateTraceId();
-        } else {
-            Tracing.setCurrent(null);
-        }
-        message.setTraceId(traceId);
+        message.setTraceId(Tracing.generateTraceId()); // tracing
 
         JChannel channel = proxy.select(_metadata);
 
