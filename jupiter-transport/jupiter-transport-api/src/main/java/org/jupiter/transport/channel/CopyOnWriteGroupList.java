4d6a835fdfffd968c6d62c2c0cd9177f7a69f3b4
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2017-02-02 21:10:17 +0800

    Initial commit

--- jupiter-transport/jupiter-transport-api/src/main/java/org/jupiter/transport/channel/CopyOnWriteGroupList.java
+++ jupiter-transport/jupiter-transport-api/src/main/java/org/jupiter/transport/channel/CopyOnWriteGroupList.java
@@ -23,9 +23,9 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * 相同服务, 不同服务节点的channel group容器,
  * 线程安全(写时复制), 实现原理类似 {@link java.util.concurrent.CopyOnWriteArrayList}.
- * <p/>
+ *
  * update操作仅支持addIfAbsent/remove, update操作会同时更新对应服务节点(group)的引用计数.
- * <p/>
+ *
  * jupiter
  * org.jupiter.transport.channel
  *
