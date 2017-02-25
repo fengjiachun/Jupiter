4b9585cadf918e3ec7128355f31888a81fa59a7f
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2017-02-26 01:53:19 +0800

    Initial commit

--- jupiter-serialization/jupiter-serialization-api/src/main/java/org/jupiter/serialization/Serializer.java
+++ jupiter-serialization/jupiter-serialization-api/src/main/java/org/jupiter/serialization/Serializer.java
@@ -31,7 +31,7 @@ public abstract class Serializer {
     /**
      * The max buffer size for a {@link Serializer} to cached.
      */
-    public static final int MAX_CACHED_BUF_SIZE = 64 * 1024;
+    public static final int MAX_CACHED_BUF_SIZE = 256 * 1024;
 
     /**
      * The default buffer size for a {@link Serializer}.
