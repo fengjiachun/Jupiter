3b72e6ed2d8ad63eee8c62c3bc2f0414d3f47844
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2017-03-04 18:32:49 +0800

    Initial commit

--- jupiter-transport/jupiter-transport-api/src/main/java/org/jupiter/transport/payload/JRequestBytes.java
+++ jupiter-transport/jupiter-transport-api/src/main/java/org/jupiter/transport/payload/JRequestBytes.java
@@ -33,6 +33,8 @@ public class JRequestBytes extends BytesHolder {
     // id在 <request, response> 生命周期内保证进程内唯一即可, 在Id对应的Response被处理完成后这个id就可以再次使用了,
     // 所以id可在 <Long.MIN_VALUE ~ Long.MAX_VALUE> 范围内从小到大循环利用, 即使溢出也是没关系的, 并且只是从理论上
     // 才有溢出的可能, 比如一个100万qps的系统把 <0 ~ Long.MAX_VALUE> 范围内的id都使用完大概需要29万年.
+    //
+    // 未来 jupiter 计划把 invokeId 限制在48位, 留出高地址的16位作为扩展字段.
     private static final AtomicLong invokeIdGenerator = new AtomicLong(0);
 
     // 用于映射 <id, request, response> 三元组
