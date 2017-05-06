7e665bdb5ebbf1d6f621d06f0c3ceb901b5d6535
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2017-05-07 02:30:24 +0800

    Initial commit

--- jupiter-common/src/main/java/org/jupiter/common/util/internal/InternalThreadLocalMap.java
+++ jupiter-common/src/main/java/org/jupiter/common/util/internal/InternalThreadLocalMap.java
@@ -16,6 +16,8 @@
 
 package org.jupiter.common.util.internal;
 
+import org.jupiter.common.util.SystemPropertyUtil;
+
 import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -62,6 +64,11 @@ public final class InternalThreadLocalMap extends RhsPadding {
 
     private static final UnsafeReferenceFieldUpdater<StringBuilder, char[]> stringBuilderValueUpdater =
             UnsafeUpdater.newReferenceFieldUpdater(StringBuilder.class.getSuperclass(), "value");
+
+    private static final int DEFAULT_STRING_BUILDER_MAX_CAPACITY =
+            SystemPropertyUtil.getInt("jupiter.internal.thread.local.string_builder_max_capacity", 1024 << 6);
+    private static final int DEFAULT_STRING_BUILDER_INITIAL_CAPACITY =
+            SystemPropertyUtil.getInt("jupiter.internal.thread.local.string_builder_initial_capacity", 512);
 
     private static final ThreadLocal<InternalThreadLocalMap> slowThreadLocalMap = new ThreadLocal<>();
     private static final AtomicInteger nextIndex = new AtomicInteger();
@@ -166,11 +173,11 @@ public final class InternalThreadLocalMap extends RhsPadding {
     public StringBuilder stringBuilder() {
         StringBuilder builder = stringBuilder;
         if (builder == null) {
-            stringBuilder = builder = new StringBuilder(512);
+            stringBuilder = builder = new StringBuilder(DEFAULT_STRING_BUILDER_INITIAL_CAPACITY);
         } else {
-            if (builder.capacity() > 65535) {
+            if (builder.capacity() > DEFAULT_STRING_BUILDER_MAX_CAPACITY) {
                 // ensure memory overhead
-                stringBuilderValueUpdater.set(builder, new char[512]);
+                stringBuilderValueUpdater.set(builder, new char[DEFAULT_STRING_BUILDER_INITIAL_CAPACITY]);
             }
             builder.setLength(0);
         }
