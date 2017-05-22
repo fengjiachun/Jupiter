5cae1126a41a1b6d86af1e0030f41d849398ff52
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2017-05-22 23:44:36 +0800

    Initial commit

--- jupiter-common/src/main/java/org/jupiter/common/util/internal/logging/FormattingTuple.java
+++ jupiter-common/src/main/java/org/jupiter/common/util/internal/logging/FormattingTuple.java
@@ -41,20 +41,14 @@ class FormattingTuple {
 
     private final String message;
     private final Throwable throwable;
-    private final Object[] argArray;
 
     FormattingTuple(String message) {
-        this(message, null, null);
+        this(message, null);
     }
 
-    FormattingTuple(String message, Object[] argArray, Throwable throwable) {
+    FormattingTuple(String message, Throwable throwable) {
         this.message = message;
         this.throwable = throwable;
-        if (throwable == null) {
-            this.argArray = argArray;
-        } else {
-            this.argArray = trimmedCopy(argArray);
-        }
     }
 
     static Object[] trimmedCopy(Object[] argArray) {
@@ -69,10 +63,6 @@ class FormattingTuple {
 
     public String getMessage() {
         return message;
-    }
-
-    public Object[] getArgArray() {
-        return argArray;
     }
 
     public Throwable getThrowable() {
