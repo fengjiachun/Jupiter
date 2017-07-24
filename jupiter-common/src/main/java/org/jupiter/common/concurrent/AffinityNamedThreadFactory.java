43ba5daf60363636f21fdbefde160d54a2146c9c
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2017-07-24 17:21:10 +0800

    Initial commit

--- jupiter-common/src/main/java/org/jupiter/common/concurrent/AffinityNamedThreadFactory.java
+++ jupiter-common/src/main/java/org/jupiter/common/concurrent/AffinityNamedThreadFactory.java
@@ -29,7 +29,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * This is a ThreadFactory which assigns threads based the strategies provided.
- * <p>
+ *
  * If no strategies are provided AffinityStrategies.ANY is used.
  *
  * Jupiter
