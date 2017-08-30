cfe961343aad3f939d97d21854b3a5a33984b787
tukangzheng <tukangzheng@gomeplus.com>
2017-08-30 17:26:38 +0800

    添加注释

--- jupiter-registry/jupiter-registry-zookeeper/src/main/java/org/jupiter/registry/zookeeper/ZookeeperRegistryService.java
+++ jupiter-registry/jupiter-registry-zookeeper/src/main/java/org/jupiter/registry/zookeeper/ZookeeperRegistryService.java
@@ -377,6 +377,8 @@ public class ZookeeperRegistryService extends AbstractRegistryService {
         ConcurrentSet<RegisterMeta.ServiceMeta> serviceMetaSet = serviceMetaMap.get(address);
         if (serviceMetaSet == null) {
             ConcurrentSet<RegisterMeta.ServiceMeta> newServiceMetaSet = new ConcurrentSet<>();
+            //如果这个key在Map中不存在的话，则把这个key和value放到Map中，然后返回null
+            //如果要是这个key之前在Map中存在的话，则不把这个key和value放到Map中，直接返回之前的key对应的value值
             serviceMetaSet = serviceMetaMap.putIfAbsent(address, newServiceMetaSet);
             if (serviceMetaSet == null) {
                 serviceMetaSet = newServiceMetaSet;
