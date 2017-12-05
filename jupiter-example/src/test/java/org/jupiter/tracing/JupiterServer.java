f4ab50c1ecfc70b1ac837f4578e739d7001cd4b7
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2017-12-05 22:08:32 +0800

    https://github.com/fengjiachun/Jupiter/issues/33

--- jupiter-example/src/test/java/org/jupiter/tracing/JupiterServer.java
+++ jupiter-example/src/test/java/org/jupiter/tracing/JupiterServer.java
@@ -42,6 +42,7 @@ public class JupiterServer {
 
             @Override
             public Tracer getTracer() {
+                // TODO
                 Tracing tracing = Tracing.newBuilder()
                         .build();
 
