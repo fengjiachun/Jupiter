112f434770cec3e2cfe43c644c5ee7c8fc00da9c
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2018-04-01 03:58:26 +0800

    jupiter.transport.codec.low_copy = true

--- jupiter-transport/jupiter-transport-api/src/main/java/org/jupiter/transport/CodecConfig.java
+++ jupiter-transport/jupiter-transport-api/src/main/java/org/jupiter/transport/CodecConfig.java
@@ -28,7 +28,7 @@ import org.jupiter.common.util.SystemPropertyUtil;
 public final class CodecConfig {
 
     private static final boolean CODEC_LOW_COPY =
-            SystemPropertyUtil.getBoolean("jupiter.transport.codec.low_copy", false);
+            SystemPropertyUtil.getBoolean("jupiter.transport.codec.low_copy", true);
 
     public static boolean isCodecLowCopy() {
         return CODEC_LOW_COPY;
