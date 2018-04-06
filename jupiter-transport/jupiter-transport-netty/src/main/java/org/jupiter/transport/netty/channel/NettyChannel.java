d8fbca7192a76819b1a9f0283776ee00a6931a8f
jiachun.fjc <jiachun.fjc@alibaba-inc.com>
2018-04-06 21:56:18 +0800

    Initial commit

--- jupiter-transport/jupiter-transport-netty/src/main/java/org/jupiter/transport/netty/channel/NettyChannel.java
+++ jupiter-transport/jupiter-transport-netty/src/main/java/org/jupiter/transport/netty/channel/NettyChannel.java
@@ -243,14 +243,14 @@ public class NettyChannel implements JChannel {
 
         @Override
         public Object backingObject() {
-            int actualWriteBytes = byteBuf.writerIndex();
+            int actualWroteBytes = byteBuf.writerIndex();
             if (nioByteBuffer != null) {
-                actualWriteBytes += nioByteBuffer.position();
+                actualWroteBytes += nioByteBuffer.position();
             }
 
-            allocHandle.record(actualWriteBytes);
+            allocHandle.record(actualWroteBytes);
 
-            return byteBuf.writerIndex(actualWriteBytes);
+            return byteBuf.writerIndex(actualWroteBytes);
         }
 
         private static ByteBuffer newNioByteBuffer(ByteBuf byteBuf, int writableBytes) {
